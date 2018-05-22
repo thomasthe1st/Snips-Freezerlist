352112b0a6f84103e7e72f689b28dff952cbcb12
Dominic <dominic.zedler.one@gmail.com>
2018-05-22 20:11:19 +0200

    initial version

---
+++ action-ShoppingList.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import ConfigParser
+from hermes_python.hermes import Hermes
+from hermes_python.ontology import *
+import io
+from shoppinglist import Shoppinglist
+
+
+def subscribe_intent_callback(hermes, intentMessage):
+    # conf = read_configuration_file(CONFIG_INI)
+    intentname = intentMessage.intent.intent_name
+    if intentname == "domi:addShoppingListItem":
+        result_sentence = shoppinglist.add_item(intentMessage)
+        hermes.publish_end_session(intentMessage.session_id, result_sentence)
+
+    elif intentname == "domi:removeShoppingListItem":
+        result_sentence = shoppinglist.remove_item(intentMessage)
+        hermes.publish_end_session(intentMessage.session_id, result_sentence)
+
+    elif intentname == "domi:isItemOnShoppingList":
+        result_sentence = shoppinglist.is_item(intentMessage)
+        hermes.publish_end_session(intentMessage.session_id, result_sentence)
+
+    elif intentname == "domi:clearShoppingList":
+        result_sentence = shoppinglist.try_clear(intentMessage)
+        if result_sentence == 1:
+            result_sentence = "Die Einkaufsliste ist schon leer."
+            hermes.publish_end_session(intentMessage.session_id, result_sentence)
+        else:
+            shoppinglist.wanted_intents = ['domi:confirmShoppingList']
+            hermes.publish_end_session(intentMessage.session_id, result_sentence,
+                                       shoppinglist.wanted_intents)
+        
+    elif intentname == "domi:confirmShoppingList":
+        if "domi:confirmShoppingList" in shoppinglist.wanted_intents:
+            shoppinglist.wanted_intents = []
+            result_sentence = shoppinglist.clear_confirmed(intentMessage)
+            hermes.publish_end_session(intentMessage.session_id, result_sentence)
+    
+    elif intentname == "domi:showShoppingList":
+        result_sentence = shoppinglist.show()
+        hermes.publish_end_session(intentMessage.session_id, result_sentence)
+
+
+if __name__ == "__main__":
+    shoppinglist = Shoppinglist()
+    with Hermes("localhost:1883") as h:
+        h.subscribe_intents(subscribe_intent_callback).start()
---
+++ shoppinglist.py
@@ -0,0 +1,84 @@
+import pickle
+import random
+
+
+class ShoppingList:
+    def __init__(self):
+        self.wanted_intents = []  # For reacting only with wanted intents
+        self.shoppinglist_path = ".shoppinglist"
+        open(self.shoppinglist_path, 'a').close()  # Create file, if not available
+        self.shoppinglist = self.read_shoppinglist()
+
+    def add_item(self, intentMessage):
+        item = intentMessage.slots.item.first().value
+        if item in self.shoppinglist:
+            response = random.choice(["{item} steht schon auf der Liste.".format(item=str(item)),
+                                      "{item} ist auf der Liste schon vorhanden.".format(item=str(item))])
+        else:
+            self.shoppinglist.append(item)
+            self.save_shoppinglist()
+            response = random.choice(["{item} wurde hinzugefügt.".format(item=str(item)),
+                                      "{item} wurde auf die Einkaufsliste geschrieben.".format(item=str(item))])
+        return response
+
+    def remove_item(self, intentMessage):
+        item = intentMessage.slots.item.first().value
+        if item in self.shoppinglist:
+            self.shoppinglist.remove(item)
+            self.save_shoppinglist()
+            response = "{item} wurde von der Einkaufsliste entfernt.".format(item=str(item))
+        else:
+            response = "{item} ist auf der Einkaufsliste nicht vorhanden.".format(item=str(item))
+        return response
+
+    def is_item(self, intentMessage):
+        item = intentMessage.slots.item.first().value
+        if item in self.shoppinglist:
+            response = "Ja, {item} steht auf der Einkaufsliste.".format(item=str(item))
+        else:
+            response = "Nein, {item} ist nicht auf der Einkaufsliste.".format(item=str(item))
+        return response
+
+    def try_clear(self):
+        if len(self.shoppinglist) > 1:
+            response = "Die Einkaufsliste enthält noch {num} Elemente." \
+                       "Bist du dir sicher?".format(num=len(self.shoppinglist))
+        elif len(self.shoppinglist) == 1:
+            response = "Die Einkaufsliste enthält noch ein Element. Bist du dir sicher?"
+        else:
+            response = 1  # Error: Shoppinglist is already empty - no dialogue start
+        return response
+
+    def clear_confirmed(self, intentMessage):
+        if intentMessage.slots.answer.first().value == "yes":
+            self.shoppinglist = []
+            self.save_shoppinglist()
+            return "Die Einkaufsliste wurde geleert."
+        else:
+            return "Die Einkaufsliste wurde nicht geleert."
+
+    def show(self):
+        if len(self.shoppinglist) > 1:
+            shoppinglist_str = ""
+            for item in self.shoppinglist[:-1]:
+                shoppinglist_str = shoppinglist_str + item + ", "
+            response = "Die Einkaufsliste enthält {items}und {last}.".format(items=shoppinglist_str,
+                                                                             last=self.shoppinglist[-1])
+        elif len(self.shoppinglist) == 1:
+            response = "Die Einkaufsliste enthält nur {item}.".format(item=self.shoppinglist[0])
+        else:  # If shoppinglist is empty
+            response = "Die Einkaufsliste ist leer."
+        return response
+
+    def read_shoppinglist(self):
+        try:
+            with open(self.shoppinglist_path, 'rb') as f:
+                itemlist = pickle.load(f)
+            return itemlist
+        except EOFError:  # if no list in file
+            return []
+
+    def save_shoppinglist(self):
+        with open(self.shoppinglist_path, "wb") as f:
+            pickle.dump(self.shoppinglist, f)
+
