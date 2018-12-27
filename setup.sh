6fecc965cb9e0f3d0a0f4a72cf6613dca8d53f03
Dr. John Zoidberg <27244574+MrJohnZoidberg@users.noreply.github.com>
2018-12-27 05:52:10 +0100

    Fixed bug with configuration file :bug:

--- setup.sh
+++ setup.sh
@@ -2,36 +2,29 @@
 
 VENV=venv
 
-if [ ! -d "$VENV" ]
+if [[ ! -d "$VENV" ]]
 then
 
     PYTHON=`which python2`
 
-    if [ ! -f $PYTHON ]
+    if [[ ! -f ${PYTHON} ]]
     then
         echo "could not find python"
     fi
-    virtualenv -p $PYTHON $VENV
+    virtualenv -p ${PYTHON} ${VENV}
 
 fi
 
-. $VENV/bin/activate
+. ${VENV}/bin/activate
 
 pip install -r requirements.txt
 
-if [ ! -f ./.shoppinglist ]; then
+if [[ ! -f ./.shoppinglist ]]; then
     touch .shoppinglist
     sudo chown _snips-skills .shoppinglist
 fi
 
-if [ -f config.ini ]
+if [[ ! -f config.ini ]]
 then
-    rm -rf config.ini
-fi
-
-if [ -f /usr/share/snips/assistant/snippets/domi.Einkaufsliste/config.ini ]
-then
-    cp /usr/share/snips/assistant/snippets/domi.Einkaufsliste/config.ini config.ini
-else
     cp config.ini.default config.ini
 fi
