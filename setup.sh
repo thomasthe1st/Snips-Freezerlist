c9e56b671ab5ac98eaf58c83d4120d834afcf7b7
Dr. John Zoidberg <27244574+MrJohnZoidberg@users.noreply.github.com>
2019-02-01 18:05:27 +0100

    Mute output of pip3 command

--- setup.sh
+++ setup.sh
@@ -28,7 +28,7 @@ then
 
     # Activate the virtual environment and install requirements.
     . $VENV/bin/activate
-    pip3 install -r requirements.txt
+    pip3 install -rq requirements.txt
 
 else
     >&2 echo "Cannot find Python 3. Please install it."
