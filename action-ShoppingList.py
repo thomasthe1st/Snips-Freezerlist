302d69124caf8bb1b501ec1ece0ef96c927e12ba
Dr. John Zoidberg <27244574+MrJohnZoidberg@users.noreply.github.com>
2019-10-30 21:27:08 +0100

    Make code more understandable

--- action-ShoppingList.py
+++ action-ShoppingList.py
@@ -3,7 +3,7 @@
 
 import configparser
 from hermes_python.hermes import Hermes
-import hermes_python.ontology.dialogue as dialogue
+from hermes_python.ontology.dialogue import DialogueConfiguration
 import io
 from shoppinglist import ShoppingList
 
@@ -11,7 +11,7 @@ from shoppinglist import ShoppingList
 USERNAME_INTENTS = "domi"
 
 
-def user_intent(intentname):
+def add_prefix(intentname):
     return USERNAME_INTENTS + ":" + intentname
 
 
@@ -29,46 +29,47 @@ def read_configuration_file(configuration_file):
 def intent_callback(hermes, intent_message):
     # conf = read_configuration_file(CONFIG_INI)
     intentname = intent_message.intent.intent_name
-    if intentname == user_intent("addShoppingListItem"):
+    if intentname == add_prefix("addShoppingListItem"):
         result_sentence = shoppinglist.add_item(intent_message)
         hermes.publish_end_session(intent_message.session_id, result_sentence)
 
-    elif intentname == user_intent("removeShoppingListItem"):
+    elif intentname == add_prefix("removeShoppingListItem"):
         result_sentence = shoppinglist.remove_item(intent_message)
         hermes.publish_end_session(intent_message.session_id, result_sentence)
 
-    elif intentname == user_intent("isItemOnShoppingList"):
+    elif intentname == add_prefix("isItemOnShoppingList"):
         result_sentence = shoppinglist.is_item(intent_message)
         hermes.publish_end_session(intent_message.session_id, result_sentence)
 
-    elif intentname == user_intent("clearShoppingList"):
+    elif intentname == add_prefix("clearShoppingList"):
         result_sentence = shoppinglist.try_clear()
         if result_sentence == "empty":
             result_sentence = "Die Einkaufsliste ist schon leer."
             hermes.publish_end_session(intent_message.session_id, result_sentence)
         else:
-            shoppinglist.wanted_intents = [user_intent("confirmShoppingList")]
-            configure_message = dialogue.DialogueConfiguration().enable_intent(user_intent("confirmShoppingList"))
-            hermes.configure_dialogue(configure_message)
+            shoppinglist.wanted_intents = [add_prefix("confirmShoppingList")]
+            dialogue_conf = DialogueConfiguration().enable_intent(add_prefix("confirmShoppingList"))
+            hermes.configure_dialogue(dialogue_conf)
+            hermes.subscribe_intent_not_recognized(intent_not_recognized_callback)
             hermes.publish_continue_session(intent_message.session_id, result_sentence,
                                             shoppinglist.wanted_intents)
         
-    elif intentname == user_intent("confirmShoppingList"):
+    elif intentname == add_prefix("confirmShoppingList"):
         shoppinglist.wanted_intents = []
         result_sentence = shoppinglist.clear_confirmed(intent_message)
         hermes.publish_end_session(intent_message.session_id, result_sentence)
     
-    elif intentname == user_intent("showShoppingList"):
+    elif intentname == add_prefix("showShoppingList"):
         result_sentence = shoppinglist.show()
         hermes.publish_end_session(intent_message.session_id, result_sentence)
 
-    elif intentname == user_intent("sendShoppingList"):
+    elif intentname == add_prefix("sendShoppingList"):
         result_sentence = shoppinglist.send()
         hermes.publish_end_session(intent_message.session_id, result_sentence)
 
 
 def intent_not_recognized_callback(hermes, intent_message):
-    configure_message = dialogue.DialogueConfiguration().disable_intent(user_intent("confirmShoppingList"))
+    configure_message = DialogueConfiguration().disable_intent(add_prefix("confirmShoppingList"))
     hermes.configure_dialogue(configure_message)
     shoppinglist.wanted_intents = []
     hermes.publish_end_session({'sessionId': intent_message.session_id,
@@ -80,5 +81,4 @@ if __name__ == "__main__":
     shoppinglist = ShoppingList(config)
     with Hermes("localhost:1883") as h:
         h.subscribe_intents(intent_callback)
-        h.subscribe_intent_not_recognized(intent_not_recognized_callback)
         h.start()
