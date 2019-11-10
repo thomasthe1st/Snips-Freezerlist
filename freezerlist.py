866a054a2d6bc7c7f48a541e83fe1f363e9a4ec7
thomasthe1st <44786668+thomasthe1st@users.noreply.github.com>
2019-11-10 20:46:14 +0100

    Update freezerlist.py

--- freezerlist.py
+++ freezerlist.py
@@ -10,20 +10,20 @@ import datetime
 import socket
 
 
-class ShoppingList:
+class FreezerList:
     def __init__(self, config):
         self.config = config
         self.wanted_intents = []  # For reacting only with wanted intents
-        self.shoppinglist_path = ".shoppinglist"
-        io.open(self.shoppinglist_path, 'a').close()  # Create file, if not available
-        self.shoppinglist = self.read_shoppinglist()
+        self.freezerlist_path = ".freezerlist"
+        io.open(self.freezerlist_path, 'a').close()  # Create file, if not available
+        self.freezerlist = self.read_freezerlist()
 
     def add_item(self, intentmessage):
         item_list = [item.value for item in intentmessage.slots.item.all()]
-        dublicate_items = [item for item in item_list if item in self.shoppinglist]
-        added_items = [item for item in item_list if item not in self.shoppinglist]
+        dublicate_items = [item for item in item_list if item in self.freezerlist]
+        added_items = [item for item in item_list if item not in self.freezerlist]
         for item in added_items:
-            self.shoppinglist.append(item)
+            self.freezerlist.append(item)
 
         response = ""
         if added_items:
@@ -35,7 +35,7 @@ class ShoppingList:
                 items_str += "{} ".format(added_items[-1])
                 word_pl_sg = "wurde"
             first_str = items_str + random.choice(["{} hinzugefügt".format(word_pl_sg),
-                                                   "{} auf die Einkaufsliste geschrieben".format(word_pl_sg)])
+                                                   "{} auf die Tiefkühlliste geschrieben".format(word_pl_sg)])
             if not dublicate_items:
                 first_str += "."
             else:
@@ -52,14 +52,14 @@ class ShoppingList:
             second_str = items_str + random.choice(["{} schon auf der Liste.".format(word_pl_sg),
                                                     "{} auf der Liste schon vorhanden.".format(word_pl_sg)])
             response += second_str
-        self.save_shoppinglist()
+        self.save_freezerlist()
         return response
 
     def remove_item(self, intentmessage):
         item_list = [item.value for item in intentmessage.slots.item.all()]
-        notlist_items = [item for item in item_list if item not in self.shoppinglist]
-        removed_items = [item for item in item_list if item in self.shoppinglist]
-        self.shoppinglist = [item for item in self.shoppinglist if item not in removed_items]
+        notlist_items = [item for item in item_list if item not in self.freezerlist]
+        removed_items = [item for item in item_list if item in self.freezerlist]
+        self.freezerlist = [item for item in self.freezerlist if item not in removed_items]
 
         response = ""
         if removed_items:
@@ -71,7 +71,7 @@ class ShoppingList:
                 items_str += "{} ".format(removed_items[-1])
                 word_pl_sg = "wurde"
             first_str = items_str + random.choice(["{} entfernt".format(word_pl_sg),
-                                                   "{} von der Einkaufsliste entfernt".format(word_pl_sg)])
+                                                   "{} von der Tiefkühlliste entfernt".format(word_pl_sg)])
             if not notlist_items:
                 first_str += "."
             else:
@@ -86,53 +86,53 @@ class ShoppingList:
                 items_str += "{} ".format(notlist_items[-1])
                 word_pl_sg = "ist"
             second_str = items_str + random.choice(["{} nicht auf der Liste.".format(word_pl_sg),
-                                                    "{} auf der Einkaufsliste nicht vorhanden.".format(word_pl_sg)])
+                                                    "{} auf der Tiefkühlliste nicht vorhanden.".format(word_pl_sg)])
             response += second_str
-        self.save_shoppinglist()
+        self.save_freezerlist()
         return response
 
     def is_item(self, intentmessage):
         item = intentmessage.slots.item.first().value
-        if item in self.shoppinglist:
-            response = "Ja, {item} steht auf der Einkaufsliste.".format(item=str(item))
+        if item in self.freezerlist:
+            response = "Ja, {item} steht auf der Tiefkühlliste.".format(item=str(item))
         else:
-            response = "Nein, {item} ist nicht auf der Einkaufsliste.".format(item=str(item))
+            response = "Nein, {item} ist nicht auf der Tiefkühlliste.".format(item=str(item))
         return response
 
     def try_clear(self):
-        if len(self.shoppinglist) > 1:
-            response = "Die Einkaufsliste enthält noch {num} Elemente." \
-                       " Bist du dir sicher?".format(num=len(self.shoppinglist))
-        elif len(self.shoppinglist) == 1:
-            response = "Die Einkaufsliste enthält noch ein Element. Bist du dir sicher?"
+        if len(self.freezerlist) > 1:
+            response = "Die Tiefkühlliste enthält noch {num} Elemente." \
+                       " Bist du dir sicher?".format(num=len(self.freezerlist))
+        elif len(self.freezerlist) == 1:
+            response = "Die Tiefkühlliste enthält noch ein Element. Bist du dir sicher?"
         else:
             response = "empty"  # Error: Shoppinglist is already empty - no dialogue start
         return response
 
     def clear_confirmed(self, intentmessage):
         if intentmessage.slots.answer.first().value == "yes":
-            self.shoppinglist = []
-            self.save_shoppinglist()
-            return "Die Einkaufsliste wurde geleert."
+            self.freezerlist = []
+            self.save_freezerlist()
+            return "Die Tiefkühlliste wurde geleert."
         else:
-            return "Die Einkaufsliste wurde nicht geleert."
+            return "Die Tiefkühlliste wurde nicht geleert."
 
     def show(self):
-        if len(self.shoppinglist) > 1:
-            shoppinglist_str = ""
-            for item in self.shoppinglist[:-1]:
-                shoppinglist_str = shoppinglist_str + str(item) + ", "
-            response = "Die Einkaufsliste enthält {items}und {last}.".format(
-                items=shoppinglist_str, last=self.shoppinglist[-1])
-        elif len(self.shoppinglist) == 1:
-            response = "Die Einkaufsliste enthält nur {item}.".format(item=self.shoppinglist[0])
-        else:  # If shoppinglist is empty
-            response = "Die Einkaufsliste ist leer."
+        if len(self.freezerlist) > 1:
+            freezerlist_str = ""
+            for item in self.freezerlist[:-1]:
+                freezerlist_str = freezerlist_str + str(item) + ", "
+            response = "Die Tiefkühlliste enthält {items}und {last}.".format(
+                items=freezerlist_str, last=self.freezerlist[-1])
+        elif len(self.freezerlist) == 1:
+            response = "Die Tiefkühlliste enthält nur {item}.".format(item=self.freezerlist[0])
+        else:  # If freezerlist is empty
+            response = "Die Tiefkühlliste ist leer."
         return response
 
     def send(self):
-        if len(self.shoppinglist) == 0:
-            return "Die Einkaufsliste ist leer. Eine Email ist daher überflüssig."
+        if len(self.freezerlist) == 0:
+            return "Die Tiefkühlliste ist leer. Eine Email ist daher überflüssig."
 
         try:
             email_dict = literal_eval(self.config['secret']['email_data'])
@@ -159,17 +159,17 @@ class ShoppingList:
         now = datetime.datetime.now()
         now_date = now.strftime("%d.%m.%Y")
         now_time = now.strftime("%H:%M Uhr")
-        msg['Subject'] = "Deine Einkaufsliste vom {date}".format(date=now_date)
+        msg['Subject'] = "Deine Tiefkühlliste vom {date}".format(date=now_date)
 
-        emailtext = "Du hast eine Email mit deiner <b>Einkaufsliste</b> bekommen, weil du es "
+        emailtext = "Du hast eine Email mit deiner <b>Tiefkühlliste</b> bekommen, weil du es "
         emailtext += "am {date} um {time} so wolltest.</br></br>".format(date=now_date, time=now_time)
         emailtext += "Hier ist die Liste:<ul>"
-        for item in self.shoppinglist:
+        for item in self.freezerlist:
             emailtext += "<li>{item}</li>".format(item=item)
         emailtext += '</ul></br></br></br>Diese Mail wurde automatisch generiert von der App '
-        emailtext += '<a href="https://console.snips.ai/store/de/skill_Va52B5v45GB">Einkaufsliste</a> '
+        emailtext += '<a href="https://console.snips.ai/store/de/skill_Va52B5v45GB">Tiefkühlliste</a> '
         emailtext += 'aus dem Snips App Store. Der Quellcode ist frei und kann auf Github '
-        emailtext += '<a href="https://github.com/MrJohnZoidberg/Snips-Einkaufsliste">hier</a> eingesehen werden. '
+        emailtext += '<a href="https://github.com/thomasthe1st/Snips-Freezer">hier</a> eingesehen werden. '
         emailtext += 'Die Anmeldedaten, die für den Versand benötigt werden, sind lokal im System, auf dem Snips '
         emailtext += 'läuft, gespeichert.'
 
@@ -190,7 +190,7 @@ class ShoppingList:
             server.login(email_dict['from'], email_dict['password'])
             text = msg.as_string()
             server.sendmail(email_dict['from'], email_dict['to'], text)
-            response = "Die Einkaufsliste wurde als Email versendet."
+            response = "Die Tiefkühlliste wurde als Email versendet."
         except smtplib.SMTPAuthenticationError:
             response = "Die Email konnte nicht versendet werden, weil die Anmeldedaten ungültig sind."
         except smtplib.SMTPRecipientsRefused:
@@ -200,14 +200,14 @@ class ShoppingList:
             server.quit()
         return response
 
-    def read_shoppinglist(self):
+    def read_freezerlist(self):
         try:
-            with io.open(self.shoppinglist_path, 'rb') as f:
+            with io.open(self.freezerlist_path, 'rb') as f:
                 itemlist = pickle.load(f)
             return itemlist
         except EOFError:  # if no list in file
             return []
 
-    def save_shoppinglist(self):
-        with io.open(self.shoppinglist_path, "wb") as f:
-            pickle.dump(self.shoppinglist, f)
+    def save_freezerlist(self):
+        with io.open(self.freezerlist_path, "wb") as f:
+            pickle.dump(self.freezerlist, f)
